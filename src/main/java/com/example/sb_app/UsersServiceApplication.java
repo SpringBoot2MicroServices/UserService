bb2cf109ad99e15addff0f1abf00b0cc90c579ce
AkuthotaRaghu1202 <akuthotaraghu@gmail.com>
2020-12-22 22:44:42 +0530

    added yml with custom port

---
+++ src/main/java/com/example/sb_app/UsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceApplication.class, args);
+    }
+
+}
